c6b2af1718b24ddcf9bce1b3e7b9a3381cd465fd
hectorconde90 <hecopa50@gmail.com>
2020-04-26 13:03:23 +0200

    Actualizacion proyecto

--- valoraciones.js
+++ valoraciones.js
@@ -3,13 +3,13 @@
 let articles = {
     movies: [   { name: 'Pulp Fiction', score: 1 },
                 { name: 'Armageddon', score: 2 },
-                { name: 'Master and commander', score: 4 },
+                { name: 'Master and commander', score: 5},
                 { name: 'Gladiator', score: 5 },
     ],
     
     'series': [ { name: 'Breaking Bad', score: 5 },
                 { name: 'House of Cards', score: 1 },
-                { name: 'Sons of Anarchy', score: 4 },
+                { name: 'Sons of Anarchy', score: 5 },
                 { name: 'Peaky Blinders', score: 4 },
     ],
 };
@@ -21,34 +21,34 @@ const movies = articles.movies;
 
 // pedir al principio una pelicula una serie y su valoracion
 
-function introducirMaterial() {
-    let nuevaPelicula = { name: '', score: 0 };
-    let nuevaSerie = { name: '', score: 0 };
+// function introducirMaterial() {
+//     let nuevaPelicula = { name: '', score: 0 };
+//     let nuevaSerie = { name: '', score: 0 };
 
 
-    //Introduccion y valoracion de pelucula
-    nuevaPelicula.name = prompt('Introduce una nueva pelicula: ');
-    nuevaPelicula.score = parseInt(prompt(`Valora ${nuevaPelicula.name}`));
+//     //Introduccion y valoracion de pelucula
+//     nuevaPelicula.name = prompt('Introduce una nueva pelicula: ');
+//     nuevaPelicula.score = parseInt(prompt(`Valora ${nuevaPelicula.name}`));
 
-    while (nuevaPelicula.score < 0 || nuevaPelicula.score > 5 || isNaN(nuevaPelicula.score)) {
-        nuevaPelicula.score = parseInt(prompt(`El valor debe estar entra 0 y 5`));
-    }
+//     while (nuevaPelicula.score < 0 || nuevaPelicula.score > 5 || isNaN(nuevaPelicula.score)) {
+//         nuevaPelicula.score = parseInt(prompt(`El valor debe estar entra 0 y 5`));
+//     }
 
-     // Introduccion y valoracion de series
-    nuevaSerie.name = prompt('Introduce una nueva serie: ');
-    nuevaSerie.score = parseInt(prompt(`Valora ${nuevaSerie.name}`));
+//      // Introduccion y valoracion de series
+//     nuevaSerie.name = prompt('Introduce una nueva serie: ');
+//     nuevaSerie.score = parseInt(prompt(`Valora ${nuevaSerie.name}`));
 
-    while (nuevaSerie.score < 0 || nuevaSerie.score > 5 || isNaN(nuevaSerie.score)) {
-        nuevaSerie.score = parseInt(prompt(`El valor debe estar entra 0 y 5`));
-    }
+//     while (nuevaSerie.score < 0 || nuevaSerie.score > 5 || isNaN(nuevaSerie.score)) {
+//         nuevaSerie.score = parseInt(prompt(`El valor debe estar entra 0 y 5`));
+//     }
 
-    movies.push(nuevaPelicula);
-    series.push(nuevaSerie);
+//     movies.push(nuevaPelicula);
+//     series.push(nuevaSerie);
 
 
-}
+// }
 
-introducirMaterial();
+// introducirMaterial();
 
 
 
@@ -66,66 +66,56 @@ console.log(imprimirArray(articles));
 
 
 // Imprimir en pantalla un título “Séries” y una lista con losnombres y valoraciones de las series
-
+// Crear la lista de las series
 function listaSeries(pSeries) {
-    document.write('<h2>Series</h2>')
-    let seriesDocument = '<div">' 
+    // document.write('')
+    let seriesDocument = '<div class="lista"><h2>Series</h2><ol>' 
     
     //Sacamos cada nombre
     for (let serie of pSeries) {
-        seriesDocument += `Nombre:  ${serie.name}  Puntuación: ${serie.score} </br></br>`
+        seriesDocument += `<li><span class="">${serie.name},    ${serie.score}</span> estrellas. </li>`
        // console.log(serie.name);
     }
-    seriesDocument += '</div>'
+    seriesDocument += '</ol></div>'
     return seriesDocument;
     
 }
 
-
-
-
-document.write(listaSeries(series));
-
-
-
-
-
 // Imprimir en pantalla un titulo Pelicula y una lista con los nombre y las valoraciones si la valoracion de la pelicula es 5 debe aparecer en negrita y verde
 function listaMovies(pMovies) {
-    document.write('<h2>Peliculas</h2>')
-    let moviesDocument = '<div">'
+    // document.write('<div class="lista">')
+    // document.write('<h2>Peliculas</h2>')
+    let moviesDocument = '<div class="lista"><h2>Peliculas</h2><ol>';
 
     //Sacamos cada nombre
     for (let movie of pMovies) {
         if (movie.score === 5) {
-            moviesDocument += `<strong style="color: green;">Nombre:  ${movie.name}  Puntuación: ${movie.score}</strong> </br></br>`
+            moviesDocument += `<li><strong style="color: green;"> ${movie.name},   ${movie.score} estrellas.</strong> </li>`;
         } else if (movie.score === 1) {
-            moviesDocument += `<strong style="color: red;">Nombre:  ${movie.name}  Puntuación: ${movie.score}</strong> </br></br>`
+            moviesDocument += `<li><strong style="color: red;">  ${movie.name},   ${movie.score} estrellas.</strong> </li>`;
         } else {
         
-            moviesDocument += `Nombre:  ${movie.name}  Puntuación: ${movie.score} </br></br>`
+            moviesDocument += `<li> ${movie.name},   ${movie.score} estrellas.</li>`;
             //console.log(movie.name);
         }
     }
-    moviesDocument += '</div>'
+    moviesDocument += '</ol></div>';
+    // document.write('</div>')
     return moviesDocument;
 
 }
 
 
-document.write(listaMovies(movies));
-// console.log(imprimirGeneroPantalla(articles));
-//console.log(articles.movies[0].score);
-
-
+// IMPRIME LAS LISTAS 
+document.write(`<div class="container cont-princ"><div class="row"><div class="col-12 col-md-6">${listaSeries(series)}</div><div class="col-12 col-md-6">${listaMovies(movies)}</div></div></div>`);
 // IMPRIMIR POR PANTALLA MEJOR PELICULA Y MEJOR SERIE
 
-//Imprime mejor pelicula
+// Imprime mejor pelicula
 
 function mejorPelicula(pPelicula) {
     let masValor = 0;
-    let pelicula = '';
-    document.write('<div><h2 class="" style="color: blue;">Mejor pelicula:</h2></div>')
+    let imprimirPelicula = '<div class="mejores" ><h2 class="" style="color: blue;">Mejor pelicula:</h2>';
+
     for (let movie of pPelicula) {
         
         if (movie.score > masValor) {
@@ -133,20 +123,18 @@ function mejorPelicula(pPelicula) {
             pelicula = movie.name;
         }
     }
-    let imprimir = `<div style="font-size: 20px; color: tomato; "> ${pelicula} </div>`
-    return imprimir;
+    imprimirPelicula += ` <p>${pelicula}</p> </div>`;
+    return imprimirPelicula;
 }
 
 
 
 
-document.write(mejorPelicula(movies));
 
 // Imprime mejor serie
 function mejorSerie(pSerie) {
     let masValor = 0;
-    let serie = '';
-    document.write('<div><h2 class="" style="color: blue;">Mejor serie:</h2></div>')
+    let imprimirSerie = '<div class="mejores"><h2 class="" style="color: blue;">Mejor serie:</h2>';
     for (let serieCount of pSerie) {
 
         if (serieCount.score > masValor) {
@@ -154,11 +142,13 @@ function mejorSerie(pSerie) {
             serie = serieCount.name;
         }
     }
-    let imprimir = `<div style="font-size: 20px; color: tomato; "> ${serie} </div>`
-    return imprimir;
+    imprimirSerie += ` <p>${serie}</p> </div>`
+    return imprimirSerie;
 }
 
-document.write(mejorSerie(series));
+
+document.write(`<div class="container"><div class="row"><div class="col-12 col-md-6">${mejorSerie(series)}</div><div class="col-12 col-md-6">${mejorPelicula(movies)}</div></div></div>`);
+
 
 
 
